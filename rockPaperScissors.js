1ef90f0e87d9d79921c7768107cfeb01d3cf5533
buentello <cbuentellogit@gmail.com>
2021-01-29 20:16:31 -0600

    getComputerChoice function implemented

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -10,3 +10,15 @@ const getUserChoice = userInput => {
   }
 }
 //console.log(user); line to prove lines 3-4 (it should not log anything)
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+    return 'rock';
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissors'
+  }
+}
